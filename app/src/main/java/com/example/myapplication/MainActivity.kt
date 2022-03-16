97bfe2cbd5d59905d38e05286bef09b3dad2e500
QuangNhat37 <pqnhat.20it8@vku.udn.vn>
2022-03-16 17:01:09 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+public class MainActivity : AppCompatActivity() {
+    private EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
